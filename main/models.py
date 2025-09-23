64057c4a57969b43236dae5c40816026622e8ede
Dzidx <dzaky.trinindito@gmail.com>
2025-09-23 19:24:05 +0700

    tugas 4

--- main/models.py
+++ main/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     CATEGORY_CHOICES = [
         ('jersey', 'Jersey'),
         ('shoes', 'Shoes'),
@@ -14,7 +16,7 @@ class Product(models.Model):
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.URLField()
-    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default ='jersey')
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='jersey')
     is_featured = models.BooleanField(default=False)
 
     def __str__(self):
